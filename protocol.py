9defc1e11501829f6b37f03abd3a654d054a771e
CreativeBuilds <nick@creativebuilds.io>
2023-09-24 16:26:54 -0400

    removes numerical and boolean hashes

--- protocol.py
+++ protocol.py
@@ -8,19 +8,14 @@ from typing import Literal
 class TextToImage( bt.Synapse ):
     images: list[ bt.Tensor ] = []
     text: str = pydantic.Field( ... , allow_mutation = False)
-    text_hash: str = pydantic.Field( ... , allow_mutation = False)
+    text_hash: str = pydantic.Field( "" , allow_mutation = False)
     negative_prompt: str = pydantic.Field( ... , allow_mutation = False)
-    negative_prompt_hash: str = pydantic.Field( ... , allow_mutation = False)
+    negative_prompt_hash: str = pydantic.Field( "" , allow_mutation = False)
     height: int = pydantic.Field( 512 , allow_mutation = False)
-    height_hash: str = pydantic.Field( ... , allow_mutation = False)
     width: int = pydantic.Field( 512 , allow_mutation = False)
-    width_hash: str = pydantic.Field( ... , allow_mutation = False)
     num_images_per_prompt: int = pydantic.Field( 1 , allow_mutation = False)
-    num_images_per_prompt_hash: str = pydantic.Field( ... , allow_mutation = False)
     seed: int = pydantic.Field( -1 , allow_mutation = False)
-    seed_hash: str = pydantic.Field( ... , allow_mutation = False)
     nsfw_allowed: bool = pydantic.Field( False , allow_mutation = False)
-    nsfw_allowed_hash: str = pydantic.Field( ... , allow_mutation = False)
     # _t: str = 'TextToImage'
     # images: list[ bt.Tensor ] = []
     # text: str = pydantic.Field( ... , allow_mutation = False)
@@ -36,7 +31,7 @@ class TextToImage( bt.Synapse ):
 
 class ImageToImage( TextToImage ):
     image: bt.Tensor = pydantic.Field( ... , allow_mutation = False)
-    image_hash: str = pydantic.Field( ... , allow_mutation = False)
+    image_hash: str = pydantic.Field( "" , allow_mutation = False)
 
     # Miners must choose how to define similarity themselves based on their model
     # by default, the strength values are 0.3, 0.7, 0.9
